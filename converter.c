fea6fc679d4d7511a434ab0dc8306d883bb1b87b
MXCALLL <idbellam12@gmail.com>
2025-01-18 16:08:57 +0100

    optimizing code and fixing norms format

--- converter.c
+++ converter.c
@@ -1,75 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   converter.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:48:11 by muidbell          #+#    #+#             */
-/*   Updated: 2025/01/03 12:29:13 by muidbell         ###   ########.fr       */
+/*   Updated: 2025/01/18 15:44:06 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	is_valid_input(const char *str)
-{
-    if (!str || !*str)
-        return (0);
-    while ((*str == ' ') || (*str >= 9 && *str <= 13))
-        str++;
-    if (*str == '-' || *str == '+')
-        str++;
-    if (!(*str >= '0' && *str <= '9'))
-        return (0);
-    while (*str)
-    {
-        if (!(*str >= '0' && *str <= '9'))
-            return (0);
-        str++;
-    }
-    return (1);
-}
-
-static int	check_overflow(long result, int sign, int next_digit)
-{
-    if ((result > INT_MAX / 10) || (result == INT_MAX / 10))
-    {
-        if (sign == 1 && (result > INT_MAX / 10 || next_digit > 7))
-            return (-1);
-        if (sign == -1 && (result > INT_MAX / 10 || next_digit > 8))
-            return (0);
-    }
-    return (1);
-}
-
 int	ft_atoi(const char *str)
 {
-    int			sign;
-    int			overflow_check;
-    int			next_digit;
-    long		result;
+	long	result;
+	int		sign;
 
-    if (!is_valid_input(str))
-        display_error();
-    sign = 1;
-    result = 0;
-    while ((*str == ' ') || (*str >= 9 && *str <= 13))
-        str++;
-    if (*str == '-' || *str == '+')
-    {
-        if (*str == '-')
-            sign = -1;
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        next_digit = *str - '0';
-        overflow_check = check_overflow(result, sign, next_digit);
-        if (overflow_check != 1)
-            display_error();
-        result = result * 10 + next_digit;
-        str++;
-    }
-    return ((int)(result * sign));
+	result = 0;
+	sign = 1;
+	while (*str == ' ' || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '+' || *str == '-')
+		sign = 44 - *str++;
+	if (!(*str >= '0' && *str <= '9'))
+		display_error();
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str++ - '0');
+		if ((sign == 1 && result > INT_MAX)
+			|| (sign == -1 && result * sign < INT_MIN))
+			display_error();
+	}
+	if (*str != '\0')
+		display_error();
+	return ((int)(result * sign));
 }
