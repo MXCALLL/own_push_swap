9bd6744b1838fcea4c422e39a5caadc0c273748d
MXCALLL <idbellam12@gmail.com>
2025-01-21 23:53:46 +0100

    save state

--- bonus/checker_bonus.c
+++ bonus/checker_bonus.c
@@ -6,18 +6,58 @@
 /*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:58:25 by muidbell          #+#    #+#             */
-/*   Updated: 2025/01/21 22:06:52 by muidbell         ###   ########.fr       */
+/*   Updated: 2025/01/21 23:52:41 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker_bonus.h"
+
+void	perform_operation(char *op, t_stack **stack_a, t_stack **stack_b, t_args *args)
+{
+	if (!ft_strncmp(op, "sa\n", 3))
+		swap_a(stack_a);
+	else if (!ft_strncmp(op, "sb\n", 3))
+		swap_b(stack_b);
+	else if (!ft_strncmp(op, "ss\n", 3))
+		swap_both(stack_a, stack_b);
+	else if (!ft_strncmp(op, "pa\n", 3))
+		push_a(stack_a, stack_b);
+	else if (!ft_strncmp(op, "pb\n", 3))
+		push_b(stack_a, stack_b);
+	else if (!ft_strncmp(op, "ra\n", 3))
+		rotate_a(stack_a);
+	else if (!ft_strncmp(op, "rb\n", 3))
+		rotate_b(stack_b);
+	else if (!ft_strncmp(op, "rr\n", 3))
+		rotate_both(stack_a, stack_b);
+	else if (!ft_strncmp(op, "rra\n", 4))
+		reverse_ra(stack_a);
+	else if (!ft_strncmp(op, "rrb\n", 4))
+		reverse_rb(stack_b);
+	else if (!ft_strncmp(op, "rrr\n", 4))
+		reverse_rboth(stack_a, stack_b);
+	else
+		display_error(NULL, args->numbers);
+}
+
+void	do_operations(t_stack **stack_a, t_stack **stack_b, t_args *args)
+{
+	char	*operations;
+
+	operations = get_next_line(0);
+	while (operations)
+	{
+		perform_operation(operations, stack_a, stack_b, args);
+		free(operations);
+		operations = get_next_line(0);
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	t_args	args;
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	char	*operations;
 
 	if (argc < 2)
 		return (0);
@@ -25,44 +65,11 @@ int	main(int argc, char **argv)
 	stack_b = NULL;
 	args = process_input(argc, argv);
 	stack_a = insert_to_stack(&stack_a, &args.numbers, args.count);
-	operations = get_next_line(0);
-	while (operations)
-	{
-		if (ft_strncmp(operations, "sa\n", 3) == 0)
-			swap_a(&stack_a);
-		else if (ft_strncmp(operations, "sb\n", 3) == 0)
-			swap_b(&stack_b);
-		else if (ft_strncmp(operations, "ss\n", 3) == 0)
-			swap_both(&stack_a, &stack_b);
-		else if (ft_strncmp(operations, "pa\n", 3) == 0)
-			push_a(&stack_a, &stack_b);
-		else if (ft_strncmp(operations, "pb\n", 3) == 0)
-			push_b(&stack_a, &stack_b);
-		else if (ft_strncmp(operations, "ra\n", 3) == 0)
-			rotate_a(&stack_a);
-		else if (ft_strncmp(operations, "rb\n", 3) == 0)
-			rotate_b(&stack_b);
-		else if (ft_strncmp(operations, "rr\n", 3) == 0)
-			rotate_both(&stack_a, &stack_b);
-		else if (ft_strncmp(operations, "rra\n", 4) == 0)
-			reverse_ra(&stack_a);
-		else if (ft_strncmp(operations, "rrb\n", 4) == 0)
-			reverse_rb(&stack_b);
-		else if (ft_strncmp(operations, "rrr\n", 4) == 0)
-			reverse_rboth(&stack_a, &stack_b);
-		else
-		{
-			free(operations);
-			display_error(NULL, args.numbers);
-		}
-		free(operations);
-		operations = get_next_line(0);
-	}
+	do_operations(&stack_a, &stack_b, &args);
 	if (is_sorted(&stack_a) && !stack_b && stack_a)
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-	free(operations);
 	free(args.numbers);
 	free_stack(&stack_a);
 	free_stack(&stack_b);
